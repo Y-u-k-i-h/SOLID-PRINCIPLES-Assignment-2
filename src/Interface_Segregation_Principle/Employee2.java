32f365a93bde85e276223965e4086eb7557bc4ec
Chitsaka <chitsaka.gari@strathmore.edu>
2025-06-10 17:32:57 +0300

    ISP princilple

--- src/Interface_Segregation_Principle/Employee2.java
+++ src/Interface_Segregation_Principle/Employee2.java
@@ -6,19 +6,16 @@ public interface Employee2 {
 
 class OfficeEmployees implements Employee2,Documents{
 
-    @Override
     public void work() {
         System.out.println("Office employee is working");
     }
 
-    @Override
     public void writeDocuments() {
         System.out.println("Office worker is writing documents ");
-
     }
 }
 class FieldEmployee2 implements Employee2 {
-    @Override
+
     public void work() {
         System.out.println("Field Employee is working");
     }
