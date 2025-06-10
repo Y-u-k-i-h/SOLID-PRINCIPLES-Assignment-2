142e73ced39ab83a9a34a57e9585d2e18f6cc0a6
Chitsaka86 <chitsaka.gari@strathmore.edu>
2025-06-10 17:53:33 +0300

    Add files via upload

---
+++ Employee2.java
@@ -0,0 +1,22 @@
+package Interface_Segregation_Principle;
+
+public interface Employee2 {
+    void work();
+}
+
+class OfficeEmployees implements Employee2,Documents{
+
+    public void work() {
+        System.out.println("Office employee is working");
+    }
+
+    public void writeDocuments() {
+        System.out.println("Office worker is writing documents ");
+    }
+}
+class FieldEmployee2 implements Employee2 {
+
+    public void work() {
+        System.out.println("Field Employee is working...");
+    }
+}
